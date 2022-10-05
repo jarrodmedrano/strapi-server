3a61433b78cd2907591c8b23824090ed44cee611
Jarrod Medrano <jmedran@gmail.com>
2022-10-04 22:36:05 -0600

    fix order

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -2,43 +2,6 @@
  * order controller
  */
 
-"use strict";
-/**
- * Order.js controller
- *
- * @description: A set of functions called "actions" for managing `Order`.
- */
-// note that this needs to be a "private" key from STRIPE
-const stripe = require("stripe")("sk_test_zovhWaIqvhMYG3FuONCe2M6c");
-module.exports = {
-  create: async (ctx: {
-    request: { body: string };
-    state: { user: { _id: any; id: any } };
-  }) => {
-    const { address, amount, dishes, token, city, state } = JSON.parse(
-      ctx.request.body
-    );
-    const stripeAmount = Math.floor(amount * 100);
-    // charge on stripe
-    const charge = await stripe.charges.create({
-      // Transform cents to dollars.
-      amount: stripeAmount,
-      currency: "usd",
-      description: `Order ${new Date()} by ${ctx.state.user._id}`,
-      source: token,
-    });
+import { factories } from "@strapi/strapi";
 
-    // Register the order in the database
-    const order = await strapi.services.order.create({
-      user: ctx.state.user.id,
-      charge_id: charge.id,
-      amount: stripeAmount,
-      address,
-      dishes,
-      city,
-      state,
-    });
-
-    return order;
-  },
-};
+export default factories.createCoreController("api::order.order");
