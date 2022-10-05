0fd4f0ded6a21cf66e28c6348b0714d0d96ec2f5
Jarrod Medrano <jmedran@gmail.com>
2022-10-04 22:56:27 -0600

    add stripe

--- dist/src/api/order/controllers/order.js
+++ dist/src/api/order/controllers/order.js
@@ -1,7 +1,38 @@
 "use strict";
 /**
- * order controller
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController("api::order.order");
+// note that this needs to be a "private" key from STRIPE
+const stripe = require("stripe")("sk_test_zovhWaIqvhMYG3FuONCe2M6c");
+module.exports = {
+    /**
+     * Create a/an order record.
+     *
+     * @return {Object}
+     */
+    create: async (ctx) => {
+        const { address, amount, dishes, token, city, state } = JSON.parse(ctx.request.body);
+        const stripeAmount = Math.floor(amount * 100);
+        // charge on stripe
+        const charge = await stripe.charges.create({
+            // Transform cents to dollars.
+            amount: stripeAmount,
+            currency: "usd",
+            description: `Order ${new Date()} by ${ctx.state.user._id}`,
+            source: token,
+        });
+        // Register the order in the database
+        const order = await strapi.services.order.create({
+            user: ctx.state.user.id,
+            charge_id: charge.id,
+            amount: stripeAmount,
+            address,
+            dishes,
+            city,
+            state,
+        });
+        return order;
+    },
+};
--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -1,7 +1,43 @@
+"use strict";
 /**
- * order controller
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
  */
+// note that this needs to be a "private" key from STRIPE
+const stripe = require("stripe")("sk_test_zovhWaIqvhMYG3FuONCe2M6c");
+module.exports = {
+  /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
 
-import { factories } from "@strapi/strapi";
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
+    const stripeAmount = Math.floor(amount * 100);
+    // charge on stripe
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: stripeAmount,
+      currency: "usd",
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token,
+    });
 
-export default factories.createCoreController("api::order.order");
+    // Register the order in the database
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
+      amount: stripeAmount,
+      address,
+      dishes,
+      city,
+      state,
+    });
+
+    return order;
+  },
+};
